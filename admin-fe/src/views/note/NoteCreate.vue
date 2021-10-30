324f6f583e8d6a863db5cccd6c35ab689c7a0fe2
linhx <nguyendinhlinhx@gmail.com>
2021-10-30 23:00:57 +0700

    Apply CField

--- admin-fe/src/views/note/NoteCreate.vue
+++ admin-fe/src/views/note/NoteCreate.vue
@@ -2,10 +2,14 @@
   <div class="mt-10 sm:mt-0">
     <div class="md:grid md:grid-cols-3 md:gap-6">
       <div class="mt-5 md:mt-0 md:col-span-2">
-        <c-input v-model="title" label="Title" name="title" />
+        <c-field label="Title">
+          <c-input v-model="title" name="title" />
+        </c-field>
       </div>
       <div class="mt-5 md:mt-0 md:col-span-1">
-        <c-input v-model="permalink" label="Permalink" name="permalink" />
+        <c-field label="Permalink">
+          <c-input v-model="permalink" name="permalink" />
+        </c-field>
       </div>
       <div class="mt-5 md:mt-0 md:col-span-3">
         <c-textarea v-model="overview" label="Overview" name="overview" />
