7bbb70d06bf2a43be2ad2596cea0387ad855e5b4
linhx <nguyendinhlinhx@gmail.com>
2021-11-05 17:04:28 +0700

    admin-fe: editor: keep state of the toolbar when right click

--- admin-fe/src/components/c-editor/RightClickBubbleTheme.ts
+++ admin-fe/src/components/c-editor/RightClickBubbleTheme.ts
@@ -2,13 +2,16 @@ import Quill from "quill";
 
 const BubbleTheme = Quill.import("themes/bubble");
 export default class RightClickBubbleTheme extends BubbleTheme {
+
   constructor(quill: any, options: any) {
     super(quill, options);
 
     quill.container.addEventListener('contextmenu', (e: Event) => {
       e.preventDefault();
-      quill.theme.tooltip.edit();
+
+      const range = quill.getSelection();
       quill.theme.tooltip.show();
+      quill.theme.tooltip.position(quill.getBounds(range));
       return false;
     });
   }
