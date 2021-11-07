8870e3cf36fd909b0feb39e5be872a634601233c
linhx <nguyendinhlinhx@gmail.com>
2021-11-07 20:51:49 +0700

    admin-fe: refreshing token

--- admin-fe/src/env.d.ts
+++ admin-fe/src/env.d.ts
@@ -8,7 +8,8 @@ declare module '*.vue' {
 }
 
 interface ImportMetaEnv {
-  readonly VITE_APP_API_URL: string
+  readonly VITE_APP_API_URL: string,
+  readonly VITE_APP_AUTH_URL: string,
 }
 
 interface ImportMeta {
--- admin-fe/src/repositories/Api.ts
+++ admin-fe/src/repositories/Api.ts
@@ -5,10 +5,68 @@ const api = axios.create({
   withCredentials: true
 });
 
+const authApi = axios.create({
+  baseURL: import.meta.env.VITE_APP_AUTH_URL,
+  withCredentials: true
+});
+
+let subscribers: ((success: boolean) => void)[] = []; // execute subscribers after refreshing token
+function addSubscriber (callback: (success: boolean) => void) {
+  subscribers.push(callback);
+}
+function onAccessTokenFetched (success: boolean) {
+  const _subscribers = subscribers.slice(0, subscribers.length);
+  subscribers = [];
+  _subscribers.forEach(callback => callback(success));
+  isRefreshingToken = false;
+}
+
+let isRefreshingToken = false;
+
+const refreshToken = () => {
+  const result = new Promise((resolve, reject) => {
+    addSubscriber((success: boolean) => {
+      if (success) {
+        resolve(true);
+      } else {
+        reject(new Error('Can not refresh token'));
+      }
+    });
+  });
+
+  if (!isRefreshingToken) {
+    isRefreshingToken = true;
+    authApi.post('/refresh-token').then(() => {
+      onAccessTokenFetched(true);
+    }).catch(({ response }) => {
+      const messages = response.data?.messages;
+      if (messages && messages[0].messageCode === 'error.refreshToken.alreadyUsed') {
+        return authApi.post('/cancel-logout', {
+          llhs: messages[0].params.llhs
+        }).then(() => {
+          onAccessTokenFetched(true);
+        }).catch(() => {
+          onAccessTokenFetched(false);
+        });
+      } else {
+        onAccessTokenFetched(false);
+      }
+    });
+  }
+  return result;
+}
+
 api.interceptors.response.use(res => {
   return res.data;
 },
 async e => {
+  const { response } = e;
+  if (response && response.status === 401) {
+    const refreshTokenSuccess = await refreshToken().catch(() => false);
+    if (refreshTokenSuccess) {
+      return api(e.response.config);
+    }
+  }
   throw e;
 });
 
