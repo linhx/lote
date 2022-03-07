be42f24adc4b9d947f55d51dd5d6c3c03bfe4a71
linhx <nguyendinhlinhx@gmail.com>
2022-03-07 22:49:59 +0700

    dynamic vue router

--- fe/src/router/index.ts
+++ fe/src/router/index.ts
@@ -1,32 +1,27 @@
 import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
 import NotesView from '../views/NotesView.vue';
-import V404View from '../views/404.vue';
 import { pathToFile } from './preFetch';
 
-let noteComp: string | undefined = '';
+const noteRoutes: RouteRecordRaw[] = []
+for (const noteChunk in __VP_HASH_MAP__) {
+  const path = noteChunk.substring(5);
+  const component = pathToFile(path);
+  if (component) {
+    noteRoutes.push({
+      path,
+      component() {
+        return import(component);
+      }
+    });
+  }
+}
+
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'NotesView',
     component: NotesView,
-    children: [
-      {
-        path: '/:permalink',
-        beforeEnter: (to, from) => {
-          noteComp = pathToFile(to.path);
-        },
-        component: () => {
-          if (noteComp) {
-            const _noteComp = noteComp;
-            noteComp = '';
-            return import(_noteComp);
-          } else {
-            return Promise.resolve(V404View);
-          }
-        }, 
-        props: route => ({ permalink: route.params.permalink })
-      }
-    ]
+    children: noteRoutes
   },
 ];
 
