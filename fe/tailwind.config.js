062a162cc3a9c3e5425a070453438f141c3afb44
linhx <nguyendinhlinhx@gmail.com>
2022-03-14 09:45:00 +0700

    fe: config Open sans as default font

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -1,10 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts}', './notes/*.{vue,js,ts}'],
   darkMode: false,
   corePlugins: {
   },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ["Open Sans", ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   variants: {
     extend: {},
