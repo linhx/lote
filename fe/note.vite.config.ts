c524779e296ee6c10dcd7160fd21af9f44a331aa
linhx <nguyendinhlinhx@gmail.com>
2022-05-02 11:06:56 +0700

    fe: add two modes: recreate note-chunk-map.js and append new to existed note-chunk-map.js

--- fe/note.vite.config.ts
+++ fe/note.vite.config.ts
@@ -4,6 +4,8 @@ import { join, resolve } from 'path';
 import fs from 'fs';
 import { fileNameWithoutExtension } from './src/utilities/FileUtils';
 const OUT_DIR = './dist';
+const MODE_BUILD_NOTE = 'build-note';
+declare const __VP_HASH_MAP__: Record<string, string>;
 
 const notes: { [name: string]: string } = {};
 fs.readdirSync('./notes').forEach(file => {
@@ -11,26 +13,39 @@ fs.readdirSync('./notes').forEach(file => {
   notes['note-' + noteName] = resolve(__dirname, './notes', file);
 });
 
-const pageToHashMap = {};
 const hashRE = /\.(\w+)\.js$/;
-const vitePressPlugin: Plugin = {
-  name: 'note-chunk',
+const vitePressPlugin = ({
+  reCreateNoteChunkMap, deployDir
+}): Plugin => {
+  let pageToHashMap = {};
+  if (!reCreateNoteChunkMap) {
+    try {
+      require(`${deployDir}/note-chunk-map.js`);
+      pageToHashMap = __VP_HASH_MAP__ || {};
+    } catch(e) {
+      // do nothing
+    }
+  }
 
-  generateBundle(_options, bundle) {
-    for (const name in bundle) {
-      const chunk = bundle[name];
-
-      if (chunk.name.startsWith('note-')) {
-        const hash = chunk.fileName.match(hashRE)![1];
-        pageToHashMap![chunk.name.toLowerCase()] = hash;
+  return {
+    name: 'note-chunk',
+    generateBundle(_options, bundle) {
+      for (const name in bundle) {
+        const chunk = bundle[name];
+  
+        if (chunk.name.startsWith('note-')) {
+          const hash = chunk.fileName.match(hashRE)![1];
+          pageToHashMap![chunk.name.toLowerCase()] = hash;
+        }
       }
+      if (!fs.existsSync(OUT_DIR)) {
+        fs.mkdirSync(OUT_DIR);
+      }
+      fs.writeFileSync(`${OUT_DIR}/note-chunk-map.js`, `__VP_HASH_MAP__ = ${JSON.stringify(pageToHashMap)}`, );
     }
-    if (!fs.existsSync(OUT_DIR)) {
-      fs.mkdirSync(OUT_DIR);
-    }
-    fs.writeFileSync(`${OUT_DIR}/note-chunk-map.js`, `__VP_HASH_MAP__ = ${JSON.stringify(pageToHashMap)}`, );
-  },
+  }
 }
+
 
 const getVueModulesFile = (deployDir) => {
   const assetsDir = deployDir.startsWith('/') ? join(deployDir, 'assets') : join(__dirname, deployDir, 'assets');
@@ -47,13 +62,20 @@ const getVueModulesFile = (deployDir) => {
   throw new Error('Maybe the DEPLOY_DIR is wrong or you forgot to build the entire project and deploy.');
 }
 
-export default ({ mode }) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+/**
+ * reuse the built vendor modules instead of build a new one.
+ * by make `vue` as external, and create `src/vue-modules.ts` to keep the original modules name.
+ * Then replace `vue` by the vue-modules.ts's built file.
+ * 
+ * TODO do the same for 'plugin-vue:export-helper' - a module from @vitejs/plugin-vue
+ * 
+ * @param config config
+ * @returns 
+ */
+const vueModuluesPlugin = ({ distDir }) => {
+  const vueModulesFile = getVueModulesFile(distDir);
 
-  const assetsDir = process.env.BUILD_MODE === 'build-note'? process.env.VITE_APP_DEPLOY_DIR : OUT_DIR;
-  const vueModulesFile = getVueModulesFile(assetsDir);
-
-  const vueModuluesPlugin: Plugin = {
+  return {
     name: 'vue-modules',
     renderChunk: (code: string, chunk, options) => {
       if (code) {
@@ -61,9 +83,20 @@ export default ({ mode }) => {
       }
     }
   }
+}
+
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  const distDir = process.env.BUILD_MODE === MODE_BUILD_NOTE? process.env.VITE_APP_DEPLOY_DIR : OUT_DIR;
+  const reCreateNoteChunkMap = process.env.BUILD_MODE !== MODE_BUILD_NOTE;
 
   return defineConfig({
-    plugins: [vue(), vueModuluesPlugin, vitePressPlugin],
+    plugins: [
+      vue(),
+      vueModuluesPlugin({ distDir }),
+      vitePressPlugin({ reCreateNoteChunkMap, deployDir: process.env.VITE_APP_DEPLOY_DIR })
+    ],
     build: {
       outDir: OUT_DIR,
       assetsDir: 'notes',
