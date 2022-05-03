5fdb9a8cf65e653003f6a379cfcfde9f3c3f925c
linhx <nguyendinhlinhx@gmail.com>
2022-05-03 15:55:00 +0700

    fe: publish single note: remove old built files

--- fe/bin/deploy-note.js
+++ fe/bin/deploy-note.js
@@ -1,17 +1,18 @@
 const fs = require('fs-extra');
+const path = require('path');
 const { loadEnv } = require('vite');
 const fileUtils = require('./FileUtils');
 process.env = {...process.env, ...loadEnv('development', process.cwd())};
 
-const newNotes = fs.readdirSync(process.env.VITE_APP_NOTE_DIR);
-const oldNotes = fs.readdirSync(process.env.VITE_APP_DEPLOY_DIR);
+const newNotes = fs.readdirSync('./dist/notes');
+const oldNotes = fs.readdirSync(process.env.VITE_APP_NOTE_DIR);
 if (oldNotes) {
-  for (const newNote in newNotes) {
+  for (const newNote of newNotes) {
     const noteName = newNote.replace(/\.(\w+)\.js$/, '');
     const regexNoteFile = new RegExp(noteName + '\\.(\\w+)\\.js$');
     oldNotes.forEach(file => {
       if(regexNoteFile.test(file)) {
-        const filePath = path.join(process.env.VITE_APP_DEPLOY_DIR, file);
+        const filePath = path.join(process.env.VITE_APP_NOTE_DIR, file);
         fileUtils.rmSyncSilent(filePath);
       }
     });
