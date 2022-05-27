034db0973fda2d4e7176f99f4692982273272b4b
linhx <nguyendinhlinhx@gmail.com>
2022-05-27 14:55:18 +0700

    fe: use font Inter instead of Open Sans

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -30,7 +30,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ["Open Sans", ...defaultTheme.fontFamily.sans],
+        'sans': ['Inter', 'sans-serif', ...defaultTheme.fontFamily.sans],
       }
     },
   },
