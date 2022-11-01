2ab1b986a8f5bfe95829c5e79e941152b6c074de
linhx <nguyendinhlinhx@gmail.com>
2022-11-01 14:16:35 +0700

    fix: dockerlize: can't publish

--- start-be.sh
+++ start-be.sh
@@ -1,2 +1,2 @@
-cd $FE_DIR && npm run build --prefix &
+cd $FE_DIR && npm run build --prefix && rm -r $VITE_APP_DEPLOY_DIR/* && cp -r $FE_DIR/dist/* $VITE_APP_DEPLOY_DIR &
 cd ./be && node ./dist/main.js
