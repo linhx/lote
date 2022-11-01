785ff7d4b1674049d70c0dc0a43978eb2b5e8914
linhx <nguyendinhlinhx@gmail.com>
2022-11-01 14:20:20 +0700

    chore: remove auth env

--- admin-fe/src/env.d.ts
+++ admin-fe/src/env.d.ts
@@ -9,8 +9,6 @@ declare module '*.vue' {
 
 interface ImportMetaEnv {
   readonly VITE_APP_API_URL: string;
-  readonly VITE_APP_AUTH_URL: string;
-  readonly VITE_APP_LOGIN_URL: string;
 }
 
 interface ImportMeta {
