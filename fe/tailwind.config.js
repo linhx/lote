da368827d3be9e080475b6b8d3c01747dd319562
linhx <nguyendinhlinhx@gmail.com>
2022-11-02 21:39:37 +0700

    chore(fe): remove unused font

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Inter', 'sans-serif', ...defaultTheme.fontFamily.sans],
+        'sans': ['sans-serif', ...defaultTheme.fontFamily.sans],
       }
     },
   },
