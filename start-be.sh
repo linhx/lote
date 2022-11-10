1228d14f8812ec048689991a6230d438691a3558
linhx <nguyendinhlinhx@gmail.com>
2022-11-10 21:54:52 +0700

    fix: avoid remove published notes when docker compose up build again

--- start-be.sh
+++ start-be.sh
@@ -2,8 +2,12 @@
 if [ "$START_MODE" == "rebuild-fe" ] || [ -z "$(ls -A $VITE_APP_DEPLOY_DIR)" ]
 then
   cd $FE_DIR && npm run build
+  cp -r $VITE_APP_DEPLOY_DIR/$FE_PUBLISHED_NOTES_DIR /apps/temp/$FE_PUBLISHED_NOTES_DIR
+  cp -r $VITE_APP_DEPLOY_DIR/$FE_PUBLISHED_TILS_DIR /apps/temp/$FE_PUBLISHED_TILS_DIR
   rm -rf $VITE_APP_DEPLOY_DIR/*
   cp -r $FE_DIR/dist/* $VITE_APP_DEPLOY_DIR
+  cp -r /apps/temp/$FE_PUBLISHED_NOTES_DIR $VITE_APP_DEPLOY_DIR/$FE_PUBLISHED_NOTES_DIR
+  cp -r /apps/temp/$FE_PUBLISHED_TILS_DIR $VITE_APP_DEPLOY_DIR/$FE_PUBLISHED_TILS_DIR
 fi
 
 cd $BE_DIR && node ./dist/main.js
