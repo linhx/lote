752a182e8966f1e330fa89b837416c84b08ca340
linhx <nguyendinhlinhx@gmail.com>
2022-11-20 23:41:07 +0700

    feat(emoji2): update css for fe; escape emoji in codeblock

--- be/src/utilities/content/highlight-code-block.ts
+++ be/src/utilities/content/highlight-code-block.ts
@@ -1,17 +1,15 @@
 import hljs from 'highlight.js';
 import * as unescapeHTML from 'underscore.string/unescapeHTML';
-import emojiUnicodeMap from './emoji-unicode-map';
 
 const REGEX_CODEBLOCK = /<pre><code class=\"language-([A-Za-z0-9\-]+)\">(.+?)<\/code><\/pre>/gs;
 
-const REGEX_EMOJI = /<span class=\"em em-([a-z0-9\-]+)">(.+?)<\/span>/gs;
+const REGEX_EMOJI = /<img class=\"em\"(.*?) alt=\"(.+?)\"(.*?)>/gs;
 
 const escapeEmoji = (codeContent) => {
   const mapEmoji = new Map();
-  const newCodeContent = codeContent.replace(REGEX_EMOJI, (match, emojiName, emojiContent) => {
-    const emojiUnicode = emojiUnicodeMap.get(emojiName);
-    mapEmoji.set(emojiUnicode, match);
-    return emojiUnicode;
+  const newCodeContent = codeContent.replace(REGEX_EMOJI, (match, _, emojiKey) => {
+    mapEmoji.set(emojiKey, match);
+    return emojiKey;
   });
   return {
     content: newCodeContent,
