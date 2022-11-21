c008e341a5e825e25d0b2b791be98bbf689c9b94
linhx <nguyendinhlinhx@gmail.com>
2022-11-21 23:06:11 +0700

    feat: update nestjs version

--- be/src/main.ts
+++ be/src/main.ts
@@ -1,6 +1,6 @@
 import { config } from 'dotenv';
-import * as dotenvExpand from 'dotenv-expand';
-dotenvExpand(config(
+import { expand } from 'dotenv-expand';
+expand(config(
   {
     path: process.env.NODE_ENV === 'development' ? '.env.dev' : undefined
   }
