dc35ec15cf298ab54e0a36787dd39209652492dd
linhx <nguyendinhlinhx@gmail.com>
2022-12-18 00:59:26 +0700

    feat(fe): add footer

--- fe/layouts/main.layout.js
+++ fe/layouts/main.layout.js
@@ -1,3 +1,4 @@
+import Footer from '@/components/shared/organisms/Footer';
 import { ModalProvider } from '@/hooks/modal';
 import Header from '../components/shared/organisms/Header';
 
@@ -9,9 +10,13 @@ export default function MainLayout({ children }) {
           className="w-full text-gray-800 dark:text-slate-200"
           innerClass="md:max-w-3xl mx-auto px-4 md:px-0"
         />
-        <div className="text-gray-800 dark:text-slate-400 px-4 md:px-0 pb-8 md:max-w-3xl mx-auto">
+        <div className="content-wrapper text-gray-800 dark:text-slate-400 px-4 md:px-0 pb-6 md:max-w-3xl mx-auto">
           {children}
         </div>
+        <Footer
+          className="w-full h-14 border-t-1 border-slate-100 dark:border-slate-800"
+          innerClass="h-full px-4 md:px-0 md:max-w-3xl mx-auto"
+        />
       </ModalProvider>
     </>
   );
